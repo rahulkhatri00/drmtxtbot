2d6279556908e29133179472532072f9c6af8f3f
ɧąƈƙɛཞ <154057000+75atulkumar@users.noreply.github.com>
2024-02-26 12:24:27 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -2,4 +2,4 @@ api_id = 28912814
 api_hash = "8c69efa7804f6f3eb167984e2f4cf549"
 bot_token = "7090258067:AAEk3VfwU3_qCY9ZoOIJfHdfR31SR9itYxE"
 AUTH = 6641182033
-#Bot Username = "@ajldkfjao"
+#Bot Username = "jarurihaiBot"
