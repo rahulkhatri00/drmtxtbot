fa4be708165e36417999cbf1345cd0b0184a4458
ɧąƈƙɛཞ <154057000+75atulkumar@users.noreply.github.com>
2024-03-02 07:37:49 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,5 +1,5 @@
 api_id = 28912814
 api_hash = "8c69efa7804f6f3eb167984e2f4cf549"
-bot_token = "6869019608:AAGTn0wi69TPpWmxkj7iw08cApKLXikUBH8"
-AUTH = 7140557396
+bot_token = "6954097304:AAGO7hXNfQXrbxG2DSyMr1NdqBx5o858KWg"
+AUTH = 6369568191
 #Bot Username = "Drmhaihehehbot"
