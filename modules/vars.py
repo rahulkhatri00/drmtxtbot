945a9db0d8e297d917abb1843f8a1c8f695bb1a7
ɧąƈƙɛཞ <154057000+75atulkumar@users.noreply.github.com>
2024-03-07 12:38:00 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,5 +1,5 @@
 api_id = 28912814
 api_hash = "8c69efa7804f6f3eb167984e2f4cf549"
-bot_token = "6887353230:AAF15fl6V88C9AbDew_Iqi0XSQR_9OuDyhs"
+bot_token = "7196688305:AAE_l1VdxLp7KDDwAShQMEvMbKIEMHFapNw"
 AUTH = 7140557396
-#Bot Username = "txtzndabot"
+#Bot Username = "nda2txtbot"
