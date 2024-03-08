444025fc6d06b8513dbd5bf2652955d767913665
ɧąƈƙɛཞ <154057000+75atulkumar@users.noreply.github.com>
2024-03-08 12:24:32 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,5 +1,5 @@
 api_id = 28912814
 api_hash = "8c69efa7804f6f3eb167984e2f4cf549"
-bot_token = "7132177928:AAFha0WrPWcvwXIwNGTMRzcOOHKSM-nuUZI"
+bot_token = "6864493153:AAF4bK_Drwu9zOLUnUYo5O4gUqYSEiLBEO8"
 AUTH = 7140557396
-#Bot Username = "nda_4_bot"
+#Bot Username = "Txtextraggctbot"
