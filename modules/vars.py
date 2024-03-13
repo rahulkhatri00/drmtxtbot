5c95f083a95f85c01432a3b98a56e4f2339d4387
ɧąƈƙɛཞ <154057000+75atulkumar@users.noreply.github.com>
2024-03-13 15:12:34 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,5 +1,5 @@
 api_id = 28912814
 api_hash = "8c69efa7804f6f3eb167984e2f4cf549"
-bot_token = "6554437159:AAHu8xcBz8WLG0WEKwkJdv87e8u66XmemEw"
+bot_token = "7103204685:AAGUTN7XOyvW-9PcZAzK-LvuMxSDiZokqsI"
 AUTH = 7140557396
-#Bot Username = "Failurehumaitobot"
+#Bot Username = "hacejjebdbbot"
