a0a03788f2c4120e213e46d995b3c4bdbb308b30
rahulkhatri00 <116899776+rahulkhatri00@users.noreply.github.com>
2024-05-06 08:51:18 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,5 +1,5 @@
 api_id = 27161113
 api_hash = "65c4b51f54049ebe22bc01fd9a95575b"
-bot_token = "6394393918:AAG353x4Yvvi8dipRFhFDW6cKHDyVCHluvc"
+bot_token = "6806206800:AAHpkIk7pL3RyvJ4EOPS5Vbk2m79u03S6Ss"
 AUTH = 6401497985
 #Bot Username = "hacejjebdbbot"
